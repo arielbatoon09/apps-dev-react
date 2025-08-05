cff57a3d9bf555127712b32ba06084d463effce9
arielbatoon09 <info.arielbatoon@gmail.com>
2025-08-05 09:25:45 +0800

    Added variable data with curly braces approach

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,30 @@
 import Button from "./Button";
 
 function Profile() {
+  const person = {
+    name: "Boss Malupiton",
+    avatar: "/arayko.jpeg",
+    role: "Content Creator",
+    description: "Joel Ravanera <b>(Boss)</b>, or known online as Malupiton, is a Filipino social media personality and entertainer. He known for his witty and relatable content in online platforms such as Facebook, TikTok, and YouTube.",
+  };
+
   return (
     <div className="cmp-profile">
       <div className="cmp-profile_header">
-        <h1>Ariel Batoon</h1>
-        <p>Software Developer</p>
+        <img
+          className="avatar"
+          src={person.avatar}
+          alt={person.name}
+        />
+        <h1>{person.name}</h1>
+        <p>{person.role}</p>
       </div>
-      <p className="cmp-profile_description">Hi there, I'm Ariel. I'm a Software Developer passionate on developing high-performance applications that seamlessly bridge web and mobile experiences.</p>
+
+      {/* If returns a vaue with a pure string */}
+      <p>{person.description}</p>
+
+      {/* If returns a value with a string */}
+      <p className="cmp-profile_description" dangerouslySetInnerHTML={{ __html: person.description }} />
       <Button />
     </div>
   )
